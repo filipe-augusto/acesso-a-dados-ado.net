d40992b74a2a383e4560d4d68c97b72b6e36449b
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-11-08 21:40:38 -0300

    ado net acesso a dados

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace baltaDataAccess
+{
+
+    class Program
+    {
+       const string connectionStirng = @"Server=192.168.99.100\sqlserver,1433;Database=balta;
+        User ID=sa; Password=1q2w3e4r@#$";
+       //se vc colocar a autenticação do windows será integrated security=sspi 
+        static void Main(string[] args)
+        {
+        //dotnet add package microsoft.data.sqlclient add o pacote
+            // Microsoft.Data.SqlClient (Nuget)
+            System.Console.WriteLine("hello word");
+        }
+
+    }
+}
