662e2d5c92a1ae161c03b4a3fb0a8d022c1c049e
Filipe Augusto Santos Chaves <fchaves@institutomonitor.com.br>
2022-11-09 18:08:23 -0300

    dapper inicio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using baltaDataAccess.Models;
+using Dapper;
 using Microsoft.Data.SqlClient;
 
 namespace baltaDataAccess
@@ -8,40 +10,46 @@ namespace baltaDataAccess
     {
         const string connectionStirng = @"Server=192.168.99.100\sqlserver,1433;Database=balta;
         User ID=sa; Password=1q2w3e4r@#$";
+        const string conectStringJob = @"Server=IM-BRS-NT1071\MSSQLSERVER01; Integrated Security=SSPI; Database=balta";
         //se vc colocar a autenticação do windows será integrated security=sspi 
         //dotnet add package microsoft.data.sqlclient add o pacote
         // Microsoft.Data.SqlClient (Nuget)
+        //dotnet add package dapper
         static void Main(string[] args)
+        {                                        
+                Dapper();
+           
+        }
+
+
+        static void Dapper(){
+        using(var con = new SqlConnection(connectionStirng)){
+            var categories =  con.Query<Category>("SELECT [Id], [Title] FROM [Category]");   
+                foreach(var category in categories) 
+                System.Console.WriteLine($"{category.Id}-{category.Title}");        
+             }
+        }
+
+        static void ModoAntigo(){
+           
+        using (var con = new SqlConnection(connectionStirng))
         {
-            #region old
-
-            using (var con = new SqlConnection(connectionStirng))
+            System.Console.WriteLine("conectado.");
+            con.Open();
+            using (var command = new SqlCommand())
             {
-                System.Console.WriteLine("conectado.");
-                con.Open();
-                using (var command = new SqlCommand())
+                command.Connection = con;
+                command.CommandType = System.Data.CommandType.Text;
+        
+                var reader = command.ExecuteReader();
+            // command.ExecuteNonQuery para proc
+                while (reader.Read())
                 {
-                    command.Connection = con;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [ID], [TITLE] FROM [CATEGORY]";
-                    var reader = command.ExecuteReader();
-                   // command.ExecuteNonQuery para proc
-                    while (reader.Read())
-                    {
-                        System.Console.WriteLine($"{reader.GetGuid(0)} - " +
-                        $"{reader.GetString(1)}");
-                    }
-
+                    System.Console.WriteLine($"{reader.GetGuid(0)} - " +
+                    $"{reader.GetString(1)}");
                 }
             }
-            /*            var connection = new SqlConnection();
-                       connection.Open();
-                       connection.Close();
-                       connection.Dispose(); */
-            #endregion
-
-
-            System.Console.WriteLine("hello word");
+        }
         }
 
     }
